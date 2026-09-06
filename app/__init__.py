53a14654c3eb1f9ef19713479fccf9eafa29225f
Yuri Teixeira <yuriteixeirac@proton.me>
2026-09-06 13:27:43 -0300

    feat: authlib config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import os
 
+from authlib.integrations.starlette_client import OAuth
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from sqlalchemy import create_engine
@@ -8,9 +9,19 @@ from sqlalchemy.orm import declarative_base
 load_dotenv()
 
 app = FastAPI()
-engine = create_engine(
-    url=os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///sqlite3.db')
+engine = create_engine(url=os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///sqlite3.db'))
+oauth = OAuth()
+
+oauth.register(
+    'suap',
+    client_id=os.getenv('SUAP_CLIENT_ID'),
+    client_secret=os.getenv('SUAP_CLIENT_ID'),
+    authorize_url='https://suap.ifrn.edu.br/o/authorize/',
+    access_token_url="https://suap.ifrn.edu.br/o/token/",
+    api_base_url="https://suap.ifrn.edu.br/api/",
 )
+
+
 Base = declarative_base()
 
 from app.auth import auth_router
