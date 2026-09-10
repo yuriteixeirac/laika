9ed85977496f25dc2d345745449f9c4cd7ccc0c5
Yuri Teixeira <yuriteixeirac@proton.me>
2026-09-10 19:11:35 -0300

    chore: renomeei os arquivos dos routers para clareza

--- app/__init__.py
+++ app/__init__.py
@@ -12,25 +12,25 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     SessionMiddleware,
-    secret_key=os.getenv('SECRET_KEY'),     # type: ignore
-    same_site='lax',
-    max_age=14 * 86400
+    secret_key=os.getenv("SECRET_KEY"),  # type: ignore
+    same_site="lax",
+    max_age=14 * 86400,
 )
 
-engine = create_async_engine(url=os.getenv('SQLALCHEMY_DATABASE_URI', ''))
+engine = create_async_engine(url=os.getenv("SQLALCHEMY_DATABASE_URI", ""))
 oauth = OAuth()
 
 oauth.register(
-    'suap',
-    client_id=os.getenv('SUAP_CLIENT_ID'),
-    client_secret=os.getenv('SUAP_CLIENT_SECRET'),
-    authorize_url='https://suap.ifrn.edu.br/o/authorize/',
+    "suap",
+    client_id=os.getenv("SUAP_CLIENT_ID"),
+    client_secret=os.getenv("SUAP_CLIENT_SECRET"),
+    authorize_url="https://suap.ifrn.edu.br/o/authorize/",
     access_token_url="https://suap.ifrn.edu.br/o/token/",
     api_base_url="https://suap.ifrn.edu.br/api/",
 )
 
 Base = declarative_base()
 
-from app.routers.auth import auth_router
+from app.routers.auth_router import auth_router
 
 app.include_router(auth_router)
